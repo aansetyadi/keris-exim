ce603d12ff680394aa06bea46dd5118e1774b267
aan edp <setya.ananta@gmail.com>
2026-09-23 14:28:44 +0700

    perbaikan laporan

--- proses/proses_pemasukan_fg.php
+++ proses/proses_pemasukan_fg.php
@@ -5,9 +5,12 @@ include "config/database.php";
     $tgl_awal  = isset($_POST['tgl_awal']) ? $_POST['tgl_awal'] : date('Y-m-01');
     $tgl_akhir = isset($_POST['tgl_akhir']) ? $_POST['tgl_akhir'] : date('Y-m-d');
 
+    //$sql = "SELECT pr.no_mutasi, TO_CHAR(tgl_mutasi, 'dd-MM-yyyy') AS tgl, pr.kode_barang, p.nama, p.satuan, pr.qty 
+    //        FROM mutasi_fg_in pr LEFT JOIN produk p ON pr.kode_barang = p.kode 
+    //       WHERE pr.tgl_mutasi BETWEEN :tgl_awal AND :tgl_akhir AND pr.status = 'TERIMA' ORDER BY pr.no_mutasi";
     $sql = "SELECT pr.no_mutasi, TO_CHAR(tgl_mutasi, 'dd-MM-yyyy') AS tgl, pr.kode_barang, p.nama, p.satuan, pr.qty 
             FROM mutasi_fg_in pr LEFT JOIN produk p ON pr.kode_barang = p.kode 
-            WHERE pr.tgl_mutasi BETWEEN :tgl_awal AND :tgl_akhir AND pr.status = 'TERIMA' ORDER BY pr.no_mutasi";
+            WHERE pr.tgl_mutasi BETWEEN:tgl_awal AND :tgl_akhir AND pr.status = 'TERIMA' ORDER BY pr.no_mutasi";
     $stmt = $conn->prepare($sql);
     $stmt->bindParam(':tgl_awal', $tgl_awal);
     $stmt->bindParam(':tgl_akhir', $tgl_akhir);
